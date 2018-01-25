a502014d98646f12b2ff3787c4c73528d500a545
Sepand <31024588+DSep@users.noreply.github.com>
2018-01-24 20:05:51 -0800

    Add files via upload
    Updated TriggerManager file for oculus.

---
+++ Rats_Testing/Assets/Scripts/TriggerManager.cs
@@ -0,0 +1,96 @@
+﻿
+using UnityEngine;
+using UnityEngine.Audio;
+
+//[RequireComponent(typeof(AudioSource))]
+
+public class TriggerManager : MonoBehaviour
+{
+	public GameObject dissectAnimal;
+	public GameObject previousDissection;
+
+	public GameObject previous;
+	private bool previousOpen = true;
+    private bool inMarker = false;
+    public int totalTime = 100000;
+
+	public GameObject rightController;
+	public GameObject leftController; // for left handed people
+
+    public GameObject scissorsClosed;
+    public GameObject scissorsOpen;
+
+    private int index = 0;
+
+    void OnTriggerEnter(Collider other)
+    {
+        //int index = (int)other.GetComponentInParent<SteamVR_TrackedObject>().index;
+
+        if (previous != null)
+            previousOpen = !previous.activeInHierarchy;// checks if the previous object is active in the hierarchy
+
+        inMarker = true;
+
+        int countTimer = 0;
+
+        //while (countTimer != totalTime)
+        //{
+        //    SteamVR_Controller.Input(index).TriggerHapticPulse(2900, Valve.VR.EVRButtonId.k_EButton_SteamVR_Touchpad); // get controller index
+        //    countTimer++;
+        //}
+
+        scissorsOpen.SetActive(true);
+        scissorsClosed.SetActive(false);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        inMarker = false;
+
+        scissorsOpen.SetActive(false);
+        scissorsClosed.SetActive(true);
+    }
+    // check inMarker
+
+    void Update ()
+	{
+
+        OVRInput.Update(); // Call before checking the input
+
+        if ( (OVRInput.GetUp(OVRInput.Button.PrimaryIndexTrigger) || OVRInput.GetUp(OVRInput.Button.SecondaryIndexTrigger)) 
+            && previousOpen && inMarker) {
+
+            //if (rightController.GetComponent<SteamVR_TrackedController>().padPressed)
+                //loopVibration(rightController);
+            //else
+                //loopVibration(leftController);
+            //Debug.Log("Open");
+
+            if (previousDissection != null)
+				previousDissection.SetActive (false);
+			
+			dissectAnimal.SetActive (true);
+			this.gameObject.SetActive (false);
+
+            scissorsOpen.SetActive(false);
+            scissorsClosed.SetActive(true);
+        }
+	}
+
+    //void loopVibration(GameObject controller)
+    //{
+    //    int count = -totalTime/2;
+    //    int index = (int)controller.GetComponent<SteamVR_TrackedController>().controllerIndex;
+     
+    //  while (count != totalTime)
+    //   {
+    //        SteamVR_Controller.Input(index).TriggerHapticPulse(2900, Valve.VR.EVRButtonId.k_EButton_SteamVR_Touchpad);
+    //        count++;
+    //   }
+    //}
+
+    //public static bool GetUp(Button virtualMask, Controller controllerMask = Controller.Active)
+    //{
+    //    return OVRManager.input.GetResolvedButtonUp(virtualMask, RawButton.None, controllerMask);
+    //}
+}
